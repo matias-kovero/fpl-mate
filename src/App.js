2e601a83b34e5cdc25a2cb9f4a08ca63215c7835
Matias <matias.kovero@gmail.com>
2021-01-05 16:04:39 +0200

    Added sidemenu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import Menu from 'react-burger-menu/lib/menus/slide';
 import './App.css';
 import PremierContext from './PremierContext/PremierContext';
 
 import Navbar     from './Navbar';
 import MainApp    from './MainApp';
 
-// https://stackoverflow.com/questions/34369951/how-to-get-add-to-home-screen-pop-up-on-site-open-in-mobile-browser
-// Get basic info of team: https://fantasy.premierleague.com/api/entry/1598128/
-
 export default function App() {
-
   const context = useContext(PremierContext);
+  const [ menuOpen, setMenu ] = useState(false);
 
   useEffect(() => {
   }, [ context ]);
 
+  // This keeps your state in sync with the opening/closing of the menu
+  // via the default means, e.g. clicking the X, pressing the ESC key etc.
+  const handleChange = (e) => {
+    setMenu(e.isOpen);
+  }
+  const closeMenu = () => {
+    setMenu(false);
+  }
+  const toggleMenu = () => {
+    setMenu(!menuOpen);
+  }
+  const findUser = () => {
+    closeMenu();
+    context.clearProfile();
+  }
+
   return (
-    <>
-      <Navbar />
-      <MainApp team={context.team} context={context} searchProfile={context.GetTeamInfo} />
-    </>
+    <div id="outher-container" style={{height: '100%'}}>
+      <Menu
+        right width={230}
+        pageWrapId="page-wrap" 
+        outerContainerId="outher-container"
+        isOpen={menuOpen}
+        onStateChange={handleChange}
+        disableAutoFocus
+        customBurgerIcon={false}
+        itemListElement="div"
+      >
+        <div id="home" className="menu-item" onClick={findUser}>
+          <div>
+            <i className="fas fa-user"></i>
+            <span>Find User</span>
+          </div>
+        </div>
+        <div id="home" className="menu-item" onClick={closeMenu}>
+          <div>
+            <i className="fas fa-cog"></i>
+            <span style={{ textDecoration:'line-through', color:'#ffffff61'}}>Settings</span>
+          </div>
+        </div>
+      </Menu>
+      <main id="page-wrap" style={{ overflow: 'auto', height: '100%'}}>
+        <Navbar toggleMenu={toggleMenu} />
+        <MainApp team={context.team} context={context} searchProfile={context.GetTeamInfo} />
+      </main>
+    </div>
   )
 }
--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -2,19 +2,23 @@ import React, { useContext } from 'react';
 import { Navbar, Form, Button } from 'react-bootstrap';
 import PremierContext from '../PremierContext/PremierContext';
 
-export default function NavBar() {
+export default function NavBar({ toggleMenu }) {
   const { clearProfile, team } = useContext(PremierContext);
 
   const clearInfo = e => {
     e.preventDefault();
-    clearProfile();
+    //clearProfile();
+    toggleMenu();
   }
   
   return (
     <Navbar collapseOnSelect expand="md" variant="dark" sticky="top" className="justify-content-between" style={{backgroundColor: '#37003c'}}>
       <Navbar.Brand className="navbar-brand-title">FPL Mate</Navbar.Brand>
       <Form inline>
-        <Button type="submit" className="navbar-searchButton" onClick={clearInfo} disabled={!team}>Find User</Button>
+        <div onClick={clearInfo} style={{color: '#FFF'}}>
+          <i className="fas fa-bars"></i>
+        </div>
+        {/*<Button type="submit" className="navbar-searchButton" onClick={clearInfo} disabled={!team}>Find User</Button>*/}
       </Form>
     </Navbar>
   )
